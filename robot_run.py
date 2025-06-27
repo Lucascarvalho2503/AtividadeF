535cd04484a711d155f9b4b95a6280ec14c42590
Lucascarvalho2503 <lucascarvalho@edu.unifil.br>
2025-06-26 23:45:07 -0300

    Corrige passagem de opções do Chrome para Robot Framework

--- robot_run.py
+++ robot_run.py
@@ -1,24 +1,34 @@
 from robot import run
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import tempfile
+import os
+
 
 user_data_dir = tempfile.mkdtemp()
 print(f"User data dir temporário: {user_data_dir}")
 
-chrome_options_list = [
-    f"--user-data-dir={user_data_dir}",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-    "--headless=new",
-    "--disable-gpu",
-]
 
 driver_path = ChromeDriverManager().install()
 print(f"ChromeDriver instalado em: {driver_path}")
 
-# Transformar lista em string de opções separadas por espaço
-chrome_options_for_robot = " ".join(chrome_options_list)
 
-run('tests/robot/',
-    variable=[f'CHROME_OPTIONS:{chrome_options_for_robot}'])
+vars_path = os.path.join(os.getcwd(), 'chrome_options_vars.py')
+with open(vars_path, 'w') as f:
+    f.write(
+        'LIST__CHROME_OPTIONS = ' +
+        repr([
+            f"--user-data-dir={user_data_dir}",
+            "--no-sandbox",
+            "--disable-dev-shm-usage",
+            "--headless=new",
+            "--disable-gpu",
+        ]) +
+        '\n'
+    )
+print(f"Arquivo de variáveis criado em: {vars_path}")
+
+
+run(
+    'tests/robot/',
+    variablefile=[vars_path]
+)
